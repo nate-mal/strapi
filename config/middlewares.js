f034dd611fdd82c0454b0d2b56264d863ee91243
nate-mal <natanaelmalita@gmail.com>
2023-04-12 22:03:13 +0300

    size

--- config/middlewares.js
+++ config/middlewares.js
@@ -25,12 +25,7 @@ module.exports = [
   {
     name: "strapi::body",
     config: {
-      formLimit: "1000mb", // modify form body
       jsonLimit: "1000mb", // modify JSON body
-      textLimit: "1000mb", // modify text body
-      formidable: {
-        maxFileSize: 1024 * 1024 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
-      },
     },
   },
 ];
