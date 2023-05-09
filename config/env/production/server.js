c2a3d9037a62619eacfcdb893f4db0e378820b9a
nate-mal <natanaelmalita@gmail.com>
2023-05-09 19:48:33 +0300

    proxy env prod

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("STRAPI_URL", "https://strapi-production-5778.up.railway.app"),
+  url: env("STRAPI_URL", "https://admin.artcafe.store"),
+  proxy: true,
   app: {
     keys: env.array("APP_KEYS"),
-    proxy: true,
   },
 });
 // see production env
