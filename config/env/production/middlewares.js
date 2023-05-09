5ad36b58744d8e2a09ae5d97b12f4533f450962b
nate-mal <natanaelmalita@gmail.com>
2023-05-09 17:14:24 +0300

    env.prod

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,34 @@
+module.exports = [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
+
+// body config ex (force bigger files)
+// {
+//   name:"strapi::body" ,
+//   config: {
+//     jsonLimit: "15mb", // modify JSON body
+//   },
+// },
---
+++ config/env/production/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("STRAPI_URL", "https://strapi-production-5778.up.railway.app"),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+});
+// see production env
